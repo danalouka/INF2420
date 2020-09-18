841b3b5a18fc4e25c01f29528ff59fab78d9577e
Dana Louka <danalouka123@gmail.com>
2020-09-18 19:42:35 -0400

    hi

--- TP1/scripts.js
+++ TP1/scripts.js
@@ -1,4 +1,4 @@
-function openTab(evt, cityName) {
+function openTab(evt, TabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -8,6 +8,6 @@ function openTab(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(TabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
