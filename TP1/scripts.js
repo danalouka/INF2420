b1faba5037884aeb088f002c72173fad955185e9
Dana Louka <danalouka123@gmail.com>
2020-09-18 20:19:14 -0400

    bug of all tabs open at the begining fixed

--- TP1/scripts.js
+++ TP1/scripts.js
@@ -1,5 +1,28 @@
+(function() {
+  
+  var i, tabcontent, tablinks; 
+
+  // doesn't display anything for every tab.
+  tabcontent = document.getElementsByClassName("tabcontent");  
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // says that none of the tabs are active
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // displays Tab "Mission" and classifying it as "active"
+  document.getElementById("Mission").style.display = "block";
+  evt.currentTarget.className += " active";
+
+})();
+
 function openTab(evt, TabName) {
-  var i, tabcontent, tablinks;
+
+  var i, tabcontent, tablinks; 
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
